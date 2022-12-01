45d04d08c5ebeb7e44d08779d45055218fa7433c
bogdanbeczkowski <bogdanbeczkowski@gmail.com>
2022-12-01 20:43:05 +0000

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,50 @@
+import java.lang.Integer.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0
+        var currentElf = 0
+        for (line in input) {
+            if (line.isNotEmpty()) {
+                currentElf += line.toInt()
+            } else {
+                maxCalories = max(currentElf, maxCalories)
+                currentElf = 0
+            }
+        }
+
+        // check the last elf
+        maxCalories = max(currentElf, maxCalories)
+
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val topThree = intArrayOf(0, 0, 0)
+        var currentElf = 0
+        for (line in input) {
+            if (line.isNotEmpty()) {
+                currentElf += line.toInt()
+            } else {
+                if (currentElf > topThree.min()) {
+                    topThree[topThree.indexOf(topThree.min())] = currentElf
+                }
+                currentElf = 0
+            }
+        }
+
+        // check the last elf
+        if (currentElf > topThree.min()) {
+            topThree[topThree.indexOf(topThree.min())] = currentElf
+        }
+
+        return topThree.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
